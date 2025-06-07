517697cc3e2918439cb1e0a63ca64bfc621ac99d
ScriptedSith <scriptedsith@gmail.com>
2025-06-07 11:33:40 -0400

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,22 +1,22 @@
-function createNavbar(ACTIVE = "Home") {
+
+function createNavbar(ACTIVE = 'Home') {
   const navbar = `
     <!-- Navigation Bar -->
     <nav class="navbar">
       <div class="nav-container">
         <ul class="nav-menu">
           <li class="nav-item">
-            <a href="index.html" class="nav-link${ACTIVE === "Home" ? " active" : ""}">Home</a>
+            <a href="index.html" class="nav-link${ACTIVE === 'Home' ? ' active' : ''}">Home</a>
           </li>
           <li class="nav-item">
-            <a href="lore.html" class="nav-link${ACTIVE === "Group Lore" ? " active" : ""}">Group Lore</a>
+            <a href="lore.html" class="nav-link${ACTIVE === 'Group Lore' ? ' active' : ''}">Group Lore</a>
             <div class="dropdown">
-              <a href="group.html" class="dropdown-link">Group</a>
               <a href="dark-council.html" class="dropdown-link">Dark Council</a>
               <a href="powerbase.html" class="dropdown-link">Powerbase</a>
             </div>
           </li>
           <li class="nav-item">
-            <a href="guides.html" class="nav-link${ACTIVE === "Guides" ? " active" : ""}">Guides</a>
+            <a href="guides.html" class="nav-link${ACTIVE === 'Guides' ? ' active' : ''}">Guides</a>
             <div class="dropdown">
               <a href="sith-imperial-policy.html" class="dropdown-link">Sith Imperial Policy</a>
               <div class="dropdown-item">
@@ -34,48 +34,66 @@ function createNavbar(ACTIVE = "Home") {
             </div>
           </li>
           <li class="nav-item">
-            <a href="holocron.html" class="nav-link${ACTIVE === "Holocron" ? " active" : ""}">Holocron</a>
+            <a href="holocron.html" class="nav-link${ACTIVE === 'Holocron' ? ' active' : ''}">Holocron</a>
             <div class="dropdown">
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">Planet History</a>
+                <a href="planets.html" class="dropdown-link has-submenu">Planet History</a>
                 <div class="sub-dropdown">
-                  <a href="https://discord.gg/73bKC82aEs" class="sub-dropdown-link">Suggest some in the discord.</a>
+                  <a class="sub-dropdown-link">Suggest some in the discord.</a>
                 </div>
               </div>
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">Battles</a>
+                <a href="battles.html" class="dropdown-link has-submenu">Battles</a>
                 <div class="sub-dropdown">
-                  <a href="https://discord.gg/73bKC82aEs" class="sub-dropdown-link">Suggest some in the discord.</a>
+                  <a class="sub-dropdown-link">Suggest some in the discord.</a>
                 </div>
               </div>
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">Weapons</a>
+                <a href="weapon.html" class="dropdown-link has-submenu">Weapons</a>
                 <div class="sub-dropdown">
-                  <a href="https://discord.gg/73bKC82aEs" class="sub-dropdown-link">Suggest some in the discord.</a>
+                  <a class="sub-dropdown-link">Suggest some in the discord.</a>
                 </div>
               </div>
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">People</a>
+                <a href="people.html" class="dropdown-link has-submenu">People</a>
                 <div class="sub-dropdown">
                   <a href="KingAdas.html" class="sub-dropdown-link">King Adas</a>
                 </div>
+                <div class="sub-dropdown">
+                  <a href="MarkaRagnos.html" class="sub-dropdown-link">Marka Ragnos</a>
+                </div>
+                <div class="sub-dropdown">
+                  <a href="NagaSadow.html" class="sub-dropdown-link">Naga Sadow</a>
+                </div>
+                <div class="sub-dropdown">
+                  <a href="TulakHord.html" class="sub-dropdown-link">Tulak Hord</a>
+                </div>
+                <div class="sub-dropdown">
+                  <a href="DarthNihilus.html" class="sub-dropdown-link">Darth Nihilus</a>
+                </div>
+                <div class="sub-dropdown">
+                  <a href="AjuntaPall.html" class="sub-dropdown-link">Ajunta Pall</a>
+                </div>
+                <div class="sub-dropdown">
+                  <a href="DarthVitiate.html" class="sub-dropdown-link">Darth Vitiate</a>
+                </div>
               </div>
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">Section of Time</a>
+                <a href="times.html" class="dropdown-link has-submenu">Section of Time</a>
                 <div class="sub-dropdown">
                   <a href="HundredYearDarkness.html" class="sub-dropdown-link">Hundred Year Darkness</a>
                 </div>
               </div>
               <div class="dropdown-item">
-                <a class="dropdown-link has-submenu">Other</a>
+                <a href="other.html" class="dropdown-link has-submenu">Other</a>
                 <div class="sub-dropdown">
-                  <a href="https://discord.gg/73bKC82aEs" class="sub-dropdown-link">Suggest some in the discord.</a>
+                  <a class="sub-dropdown-link">Suggest some in the discord.</a>
                 </div>
               </div>
             </div>
           </li>
           <li class="nav-item">
-            <a href="divisions.html" class="nav-link${ACTIVE === "Divisions" ? " active" : ""}">Divisions</a>
+            <a href="divisions.html" class="nav-link${ACTIVE === 'Divisions' ? ' active' : ''}">Divisions</a>
              <div class="dropdown">
                 <a href="dhg.html" class="dropdown-link">Dark Honor Guard</a>
                 <a href="iq.html" class="dropdown-link">Inquisitors</a>
@@ -86,7 +104,7 @@ function createNavbar(ACTIVE = "Home") {
              </div>
           </li>
           <li class="nav-item">
-            <a href="links.html" class="nav-link${ACTIVE === "Links" ? " active" : ""}">Links</a>
+            <a href="links.html" class="nav-link${ACTIVE === 'Links' ? ' active' : ''}">Links</a>
           </li>
         </ul>
       </div>
