860eaf809fcebb03c818bd0d9ea02c713677eec5
Michael <juicea313@gmail.com>
2026-03-02 13:23:47 -0500

    Updated Ranks

---
+++ guides/ranks/ranks-data.js
@@ -0,0 +1,40 @@
+// ranks-data.js
+export const RANK_DATA = [
+    {
+        category: "Low Ranks",
+        ranks: [
+            { name: "Grotthu", desc: 'Join the <a href="https://www.roblox.com/groups/7440419/The-Sith-Brotherhood#!/about">group</a>.' },
+            { name: "Hopeful", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' },
+            { name: "Acolyte", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' },
+            { name: "Sith Tyro", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' }
+        ]
+    },
+    {
+        category: "Mid Ranks",
+        ranks: [
+            { name: "Sith Initiate", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' },
+            { name: "Sith Apprentice / Adept", desc: 'Apprentice: Secure a Master. <br> Adept: Attend a <a href="../hosting/events/">Promotional Event</a>.' },
+            { name: "Sith Marauder", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' },
+            { name: "Sith Eradicator", desc: 'Attend a <a href="../hosting/events/">Promotional Event</a>.' }
+        ]
+    },
+    {
+        category: "High Ranks",
+        ranks: [
+            { name: "Sith Knight", desc: 'Eradicator for 3 days. Pass a <a href="../hosting/trials/">Knight Trial</a>.' },
+            { name: "Sith Overseer", desc: 'Knight for 5 days. <br> Host 7 <a href="../hosting/events/">Promotional Events</a>. <br> Pass an <a href="../hosting/trials/">Overseer Trial</a>.' },
+            { name: "Sith Master", desc: 'Overseer for 7 days. <br> Host 10 <a href="../hosting/events/">Promotional Events</a>. <br> Pass a <a href="../hosting/trials/">Master Trial</a>.' },
+            { name: "Sith Lord", desc: 'Master for 10 days. <br> Host 15 <a href="../hosting/events/">Promotional Events</a>. <br> Pass a <a href="../hosting/trials/">Lord Trial</a>.' },
+            { name: "Darth", desc: 'Lord for 14 days. <br> Host 25 <a href="../hosting/events/">Promotional Events</a>. <br> Council nomination and approval. <br> Pass a <a href="../hosting/trials/">Darth Trial</a>.' },
+            { name: "Dark Council", desc: 'Be voted in by 2/3 of DC+.' },
+        ]
+    },
+    {
+        category: "High Command",
+        ranks: [
+            { name: "The Emperor's Powerbase", desc: 'Chosen by the Emperor or The Dark Side.' },
+            { name: "Sith Emperor", desc: 'Chosen by The Dark Side.' },
+            { name: "The Dark Side", desc: '<span style="font-style: italic;">Unobtainable.</span>' }
+        ]
+    }
+];
