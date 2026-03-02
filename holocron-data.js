d39b1644ce70dcfe699365517fcccf11d8e3fbb0
Michael <juicea313@gmail.com>
2026-03-01 22:14:05 -0500

    Adjust

--- holocron-data.js
+++
@@ -1,9 +0,0 @@
-export const HOLOCRON_PEOPLE = [
-    { name: "King Adas", path: "KingAdas" },
-    { name: "Marka Ragnos", path: "MarkaRagnos" },
-    { name: "Naga Sadow", path: "NagaSadow" },
-    { name: "Tulak Hord", path: "TulakHord" },
-    { name: "Darth Nihilus", path: "DarthNihilus" },
-    { name: "Ajunta Pall", path: "AjuntaPall" },
-    { name: "Darth Vitiate", path: "DarthVitiate" }
-];
